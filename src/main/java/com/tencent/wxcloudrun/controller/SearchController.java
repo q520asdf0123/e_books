1691def030fc142df004fe680089491bd0589db2
Martin <martin123>
2022-01-31 01:20:29 +0800

    Update:book

--- src/main/java/com/tencent/wxcloudrun/controller/SearchController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/SearchController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -40,14 +42,56 @@ public class SearchController {
     }
 
     @PostMapping("/selectInfo")
-    @ResponseBody
-    public Url selectInfo(@RequestBody Url url, Model model) {
-        url.setUrl("https://www.pdfdrive.com" + url.getUrl());
-        return url;
+    public String selectInfo(@RequestBody Url url, Model model) {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("https://www.pdfdrive.com" + url.getUrl(), String.class);
+        Document parse = Jsoup.parse(forEntity.getBody());
+        Element previewButtonMain = parse.getElementById("previewButtonMain");
+        Elements byAttribute = previewButtonMain.getElementsByAttribute("data-preview");
+        String attr = byAttribute.attr("data-preview");
+
+        String[] split = attr.split("\\?");
+        String id = split[1].split("&")[0].split("=")[1];
+        String session = split[1].split("&")[1].split("=")[1];
+
+        Download download = new Download();
+
+        download.setPdfUrl(id, session);
+        download.setMobiUrl(id, session);
+        download.setEpubUrl(id, session);
+        model.addAttribute("download", download);
+        return "download";
     }
+
+    private static String downloadUrl(String id, String session, String type) {
+        return "https://www.pdfdrive.com/download.pdf?" +
+                "id=" + id +
+                "&h=" + session +
+                "&u=cache&ext=" +
+                type;
+    }
+
 
     @Data
     public static class Url {
         private String url;
+    }
+
+    @Data
+    public static class Download {
+        private String pdfUrl;
+        private String epubUrl;
+        private String mobiUrl;
+
+        public void setPdfUrl(String id, String session) {
+            pdfUrl = downloadUrl(id, session, "pdf");
+        }
+
+        public void setEpubUrl(String id, String session) {
+            epubUrl = downloadUrl(id, session, "epub");
+        }
+
+        public void setMobiUrl(String id, String session) {
+            mobiUrl = downloadUrl(id, session, "mobi");
+        }
     }
 }
